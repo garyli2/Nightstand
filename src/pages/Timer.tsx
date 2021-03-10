e831b7a91993248c9b45b2950d16eaa215b70a09
Gary Li <gary.li1@uwaterloo.ca>
2021-03-09 23:42:08 -0500

    Move css rules into individual files

--- src/pages/Timer.tsx
+++ src/pages/Timer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styles from './css/Timer.css';
 
 export default function Timer() {
-    return <div className={styles.timerContainer}>Timer</div>;
+    return <div className="timerContainer">Timer</div>;
 }
