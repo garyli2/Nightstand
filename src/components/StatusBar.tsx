15fcadb0c1ac94451f0d8c4a36a4850ceb55f476
Gary Li <gary.li1@uwaterloo.ca>
2021-03-29 20:49:03 -0400

    Re-render when wifi status has changed, so the wifi indicator is always up to date
    Previously, if wifi status changed after render, it wouldn't be updated until the user went on a new page.

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BiWifi, BiWifiOff } from 'react-icons/bi';
 
 export default function StatusBar() {
+    const [wifiEnabled, setWifiEnabled] = useState(navigator.onLine);
+
+    function onlineEventHandler() {
+        setWifiEnabled(true);
+    }
+
+    function offlineEventHandler() {
+        setWifiEnabled(false);
+    }
+    
+    useEffect(() => {
+        window.addEventListener('online', onlineEventHandler);
+        window.addEventListener('offline', offlineEventHandler);
+
+        return () => {
+            window.removeEventListener('online', onlineEventHandler);
+            window.removeEventListener('offline', offlineEventHandler);
+        }
+    }, [navigator.onLine]);
+
     return (
         <div className="statusBarContainer">
-            {navigator.onLine ? (
+            {wifiEnabled ? (
                 <BiWifi className="wifiIndicatorIcon" size={30} />
             ) : (
                 <BiWifiOff className="wifiIndicatorIcon" size={30} />
